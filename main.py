1ad34fa01fec036f27da9be91d642e643d294d80
Wago <wolod2011@mail.ru>
2021-08-24 14:39:44 +0300

    import_in_data

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ import random
 
 # url = "https://health-diet.ru/table_calorie/"
 #
-# headers = {
-#     'Accept': '*/*',
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-#     'Chrome/92.0.4515.159 Safari/537.36'
-#
-# }
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+    'Chrome/92.0.4515.159 Safari/537.36'
+
+}
 # req = requests.get(url, headers=headers)
 # src = req.text
 #
@@ -39,9 +39,19 @@ import random
 with open('all_categories_dict.json') as f:
     all_categories = json.load(f)
 
+    
+count = 0
 for category_name, category_href in all_categories.items():
     rep = [",", ", ", "-", " ", "'"]
     for i in rep:
         if i in category_name:
             category_name = category_name.replace(i, '_')
-    print(category_name)
+    # print(category_name)
+
+    req = requests.get(url=category_href, headers=headers)
+    src = req.text
+
+    with open(f'data/{count}_{category_name}.html', 'w', encoding='utf-8') as f:
+        f.write(src)
+
+    count += 1
