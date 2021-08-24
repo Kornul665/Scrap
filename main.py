a36f04404af40d3848793943b4b744a78de9d455
Wago <wolod2011@mail.ru>
2021-08-24 12:34:05 +0300

    'comment_code'

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from datetime import datetime
 import time
 import random
 
-url = "https://health-diet.ru/table_calorie/"
+# url = "https://health-diet.ru/table_calorie/"
 
-headers = {
-    'Accept': '*/*',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-    'Chrome/92.0.4515.159 Safari/537.36'
+# headers = {
+#     'Accept': '*/*',
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+#     'Chrome/92.0.4515.159 Safari/537.36'
 
-}
-req = requests.get(url, headers=headers)
-src = req.text
-# print(src)
+# }
+# req = requests.get(url, headers=headers)
+# src = req.text
+# # print(src)
 
-with open('index.html', 'w', encoding='utf-8') as f:
-    f.write(src)
+# with open('index.html', 'w', encoding='utf-8') as f:
+#     f.write(src)
